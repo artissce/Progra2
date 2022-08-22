20fd36a07ac4a41a4a1ce2349fccb78b027e65a1
artiss_ce <103614206+artissce@users.noreply.github.com>
2022-08-22 09:40:50 -0500

    Add files via upload

---
+++ unidad1/bidiDinaEstruc/libreria.h
@@ -0,0 +1,17 @@
+#include <stdio.h> //FILE
+#include <time.h>
+#include <stdlib.h>
+#include <string.h> //strlen -> tama?o de cadena
+#include <conio.h>
+#include <windows.h>
+
+typedef struct{
+	int matricula;
+	char nombre[20];
+	int edad;
+	char carrera[20];
+	float calif[3];
+}Dato;
+
+#include "vali.h"
+#include "funciones.h"
