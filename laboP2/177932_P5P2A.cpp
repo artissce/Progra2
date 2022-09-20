24314aee6f05f455f2411e327559bf6e10d7ea6a
artissce <artis.2004.07@gmail.com>
2022-09-20 17:53:37 -0500

    si

--- laboP2/177932_P5P2A.cpp
+++ laboP2/177932_P5P2A.cpp
@@ -3,6 +3,10 @@
 #include <time.h>
 #include <conio.h>
 #include <windows.h>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+using namespace std;
 
 //ESTRUCTURA
 typedef struct{//estructura con la informacion de los alumnos
@@ -11,34 +15,29 @@ typedef struct{//estructura con la informacion de los alumnos
  int id_al;//id unico para el inscrito
 }TipoAl; 
 
-int n, numIns;//declaracion de variables globales
+//VARIABLES GLOBALES	
+int n;//variable aleaotoria que se usara en la cantidad de grupos
+int t[5];//auxiliar
+//arreglo estatico para definir las materias/clases
+char clases[5][15]={{"PROGRAMACION"},{"ELECTRONICA"},{"ARTES"},{"DIBUJO"},{"COSTURA"}};
+TipoAl grupos[5][5]; //arreglo bidi de la estructura
 
 //DECLARACION DE FUNCIONES
-void capturaIns(TipoAl **grupos_insc, int n,int a[],char clases[5][15]);
-void generaID(TipoAl **grupos_insc, int n,int a[],char clases[5][15]);
-void impresionUno(TipoAl **grupos_insc, int n);
+void capturaIns();
+int generaID();
+void impresionUno();
 void encabezado();
 
 //FUNCION PRINCIPAL
 int main(){
-	encabezado();
-	//arreglo estatico para definir las materias/clases
-	char clases[5][15]={{"PROGRAMACION"},{"ELECTRONICA"},{"ARTES"},{"DIBUJO"},{"COSTURA"}}; 
+	encabezado();//funcion que imprime mis datos
 	srand(time(NULL));//semilla para numero aleaotorio
-	n=2+rand()%4;//variables enteras para el numero aleaotrorio con el rango seleccionado
-	//n sera para las cantidades aleaotorias de clases, de la cual se podra saber la cantidad de memoria para reservar
-	int a[n]={0};//arreglo auxiliar
-	printf("Grupos creados: %d\n",n);
-	//grupos_inst = (TipoAl **)malloc( n * sizeof(TipoAl *));
-	TipoAl *(grupos_insc);//definir el apuntador
-	for(int i=0; i<n; i++){
-		//doble apuntador que representa los grupos por alumno del tipo de la estructura
-		grupos_insc = (TipoAl *)malloc(sizeof(TipoAl));//reserva de memoria con el doble apuntador global
+	n=2+rand()%4;//GENERA EL NUMERO DE GRUPOS	
+	for(int i=0;i<n;i++){//sigue el cilco hasta el numero de grupos
+		t[i]=2+rand()%4;//genera columnas
 	}
-	
-	//capturaIns(grupos_insc, n,a,clases);
-	//impresionUno(grupos_insc,n,a);
-	
+	capturaIns();//llama a funcion para ingresar datos del alumno
+	impresionUno();//imprime los datos capturados
 	return 0;
 }
 
@@ -51,59 +50,102 @@ void encabezado(){
 	printf("\t13/09/2022\n");
 	printf("\t---------------------------\n");
 }
-/*
+
 //funcion encargada del llenado de los datos
-void capturaIns(TipoAl **grupos_insc, int n,int a[],char clases[5][15]){
+void capturaIns(){
+	int num=1;
 	for(int i=0; i<n; i++){//ciclo para llenar hasta las n grupos
-		numIns=2+rand()%4;
-		a[i]=numIns;//numIns es para la cantidad de inscritos en cada grupo
-		(*grupos_insc+i) = (TipoAl *)malloc(a[i]*sizeof(TipoAl));
-		for(int j=0;j<a[i];j++){
-			int rand=12+rand%24;//genera num aleaotorio
-			printf("Nombre: ");scanf("%s",(*grupos_insc[i][j]).nombre);//apuntador apuntando a la variable nombre para guardar info
-			(*grupos_insc[i][j]).edad=rand;//edad asignado al numero aleaotoriio
-			generaID(grupos_insc,a,clases);//llama a la funcion para tener el id
+		for(int j=0;j<t[i];j++){//columnas
+			cout<<endl<<"Ingresa nombre del alumno: ";
+			gets(grupos[i][j].nom_al);//imprime nombre almuno
+			grupos[i][j].edad=12+rand()%24;//imprime la edad entre el rango de 12 a 25 
+			grupos[i][j].id_al=generaID();//llama a la funcion generaID para darle el valor a la variable id
 		}	
 	}
 }
 
 //funcion para generar el id de acuerdo al grupo
-void generaID(TipoAl **grupos_insc, int n,int a[],char clases[5][15]){
-	//zzzz ay no se que hacer:c
-	int id=0;
-	for(int i=0; i<n; i++){
-		for(int j=0;j<a[i];j++){
-			if(clases[i][j]=="PROGRAMACION"){
-				id=100+rand%12;
-				(*grupos_insc[i][j]).id_al=id;
-			}
-			if(clases[i][j]=="ELECTRONICA"){
-				id=120+rand%12;
-				(*grupos_insc[i][j]).id_al=id;
-			}
-			if(clases[i][j]=="ARTES"){
-				id=140+rand%12;
-				(*grupos_insc[i][j]).id_al=id;
-			}
-			if(clases[i][j]=="DIBUJO"){
-				id=160+rand%12;
-				(*grupos_insc[i][j]).id_al=id;
-			}
-			if(clases[i][j]=="COSTURA"){
-				id=180+rand%12;
-				(*grupos_insc[i][j]).id_al=id;
-			}
+int generaID(){
+	int aux[11], id, num;
+	bool bandera = true;
+	for(int i=0; i<n; i++){//hace el ciclo de acuerdo al tamano de filas
+		switch(i){
+			case 0:
+				do{
+					num = 100+rand()%12;
+					if(aux[num] == 0){
+						aux[num] = 1;
+						id = num;//creo que el error esta aca
+						bandera = true;
+					}else {
+						bandera = false;
+					}
+				}while(bandera == false);
+			break;
+			case 1:
+				do{
+					num = 120+rand()%12;
+					if(aux[num] == 0){
+						aux[num] = 1;
+						id = num;
+						bandera = true;
+					}else {
+						bandera = false;
+					}
+				}while(bandera == false);
+			break;
+			case 2:
+				do{
+					num = 140 + rand()%12;
+					if(aux[num] == 0){
+						aux[num] = 1;
+						id = num;
+						bandera = true;
+					}else {
+						bandera = false;
+					}
+				}while(bandera == false);
+			break;
+			case 3:
+				do{
+					num = 160 + rand()%12;
+					if(aux[num] == 0){
+						aux[num] = 1;
+						id = num;
+						bandera = true;
+					}else {
+						bandera = false;
+					}
+				}while(bandera == false);
+			break;
+			case 4:
+				do{
+					num = 180+ rand()%12;
+					if(aux[num] == 0){
+						aux[num] = 1;
+						id = num;
+						bandera = true;
+					}else {
+						bandera = false;
+					}
+				}while(bandera == false);
+			break;
 		}
 	}
+	return id;
 }
 
 //funcion encargada de imprimir los n grupos con sus datos
-void impresionUno(TipoAl **grupos_insc, int n,int a[]){
-	for(int i=0; i<n; i++){
-		for(int j=0;j<a[i];j++){
-			printf("%s\t", (*grupos_insc[i][j]).nombre);	
-			printf("%d\t",(*grupos_insc[i][j]).edad);	
-			printf("%d\t", (*grupos_insc[i][j]).id_al);
+void impresionUno(){
+	cout<<endl<<"\t\t"<<"GRUPOS CREADOS: "<<n;
+	for(int i=0; i<n; i++){//respeta el ciclo hasta la cantidad de grupos
+		cout<<endl<<"Materia: "<<clases[i]<<"\tInscriptos: "<<t[i];
+		cout<<endl<<"Nombre\tEdad\tID\n"<<n;
+		for(int j=0;j<t[i];j++){//imprime hasta el tamano de las columnas
+			cout<<grupos[i][j].nom_al;
+			cout<<"\t"<<grupos[i][j].edad;
+			cout<<"\t"<<grupos[i][j].id_al<<endl;
 		}		
+		cout<<endl;
 	}
-}*/
+}
